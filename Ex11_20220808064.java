6c47a993bda47f4310b8983193116af1230e7a62
Avassaa <119755728+Avassaa@users.noreply.github.com>
2023-06-28 17:52:12 +0300

    Update Ex11_20220808064.java

--- Ex11_20220808064.java
+++ Ex11_20220808064.java
@@ -1,42 +1,34 @@
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class Ex11_20220808064 {
-    public static void main(String[] args) {
-
-        //  String a ="welcome";
-        //  String b="c";
-        //    System.out.println(isSubString(a,b));
-
-        //    int[] test={124,235,24,6435,745,73,25,1234,1,1,1,1,153,153,24,24};
-        // findRepeats(test,2);
-        int[] triplets = {-2, 0, 1, 3};
-        System.out.println(numOfTriplets(triplets, 2));
-
-        //    System.out.println(subSequence("Welcome"));
+    public static void main(String [] yamdux){
+        int [] test={1,12,1,1,1,1,1,346,347,34,634,26324,76,347,3457,3216,3457,45689,3};
+        findRepeats(test,0);
     }
-
-
     public static int numOfTriplets(int[] arr, int sum) {
-        int count = 0;
-        int n = arr.length;
+
+        sort(arr,new int[arr.length],0,arr.length-1);
+        int numOfTriplets = 0;
+        int len = arr.length;
 
 
-        for (int i = 0; i < n - 2; i++) {
-            int left = i + 1;
-            int right = n - 1;
+        for (int i = 0; i < len - 2; i++) {
+            int low = i + 1;
+            int high = len - 1;
 
-            while (left < right) {
-                int tripletSum = arr[i] + arr[left] + arr[right];
+            while (low < high) {
+                int tripletSum = arr[i] + arr[low] + arr[high];
                 if (tripletSum < sum) {
-                    count += right - left;
-                    left++;
+                    numOfTriplets += high - low;
+                    low++;
                 } else {
-                    right--;
+                    high--;
                 }
             }
         }
 
-        return count;
+        return numOfTriplets;
     }
 
     public static void bubbleSort(int[] arr) {
@@ -95,11 +87,8 @@ public class Ex11_20220808064 {
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < arr.length; i++) {
             Integer i1 = arr[i];
-            if (map.containsKey(i1))
-                map.put(i1, map.get(i1) + 1);
-            else {
-                map.put(i1, 1);
-            }
+            map.putIfAbsent(i1,1);
+            map.put(i1,map.get(i1)+1);
         }
         for (Integer i : map.keySet()) {
             if (map.get(i) > n) {
@@ -109,64 +98,7 @@ public class Ex11_20220808064 {
         System.out.println();
     }
 
-    /* public static void merge(int arr[], int l, int m, int r) {
 
-         int n1 = m - l + 1;
-         int n2 = r - m;
-
-         int L[] = new int[n1];
-         int R[] = new int[n2];
-
-         for (int i = 0; i < n1; ++i)
-             L[i] = arr[l + i];
-         for (int j = 0; j < n2; ++j)
-             R[j] = arr[m + 1 + j];
-
-         int i = 0, j = 0;
-
-         int k = l;
-         while (i < n1 && j < n2) {
-             if (L[i] <= R[j]) {
-                 arr[k] = L[i];
-                 i++;
-             }
-             else {
-                 arr[k] = R[j];
-                 j++;
-             }
-             k++;
-         }
-
-
-         while (i < n1) {
-             arr[k] = L[i];
-             i++;
-             k++;
-         }
-
-
-         while (j < n2) {
-             arr[k] = R[j];
-             j++;
-             k++;
-         }
-     }
-
-     public static void mergeSort(int arr[], int l, int r)
-     {
-         if (l < r) {
-
-
-             int m = l + (r - l) / 2;
-
-
-             mergeSort(arr, l, m);
-             mergeSort(arr, m + 1, r);
-
-
-             merge(arr, l, m, r);
-         }
-     }*/
     public static void sort(int[] arr, int[] temporary, int low, int high) {
         if (low < high) {
             int mid = low + (high - low) / 2;  // 1 2 3 4 5 6 7 8 9
